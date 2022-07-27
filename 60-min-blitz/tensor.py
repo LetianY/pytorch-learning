31fab1740db22034e8e8a46b8429dc0a1c159c5b
LetianY <45024501+LetianY@users.noreply.github.com>
2022-07-27 11:55:02 +0800

    Update tensor.py

--- 60-min-blitz/tensor.py
+++ 60-min-blitz/tensor.py
@@ -51,3 +51,23 @@ print(f"tensor @ tensor.T \n {tensor @ tensor.T}")
 print(tensor, "\n")
 tensor.add_(5)
 print(tensor)
+
+
+########################################################################################################
+# Tensors on the CPU and NumPy arrays can share their underlying memory locations, and changing one will change the other.
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+# NumPy array to Tensor: Changes in the NumPy array reflects in the tensor.
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
